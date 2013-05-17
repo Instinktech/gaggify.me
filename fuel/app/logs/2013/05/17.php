c4beae1a832bb4dc2d89ca06079fa362914559ed
Muhammad <m.ibrahim@logicait.pk>
2013-05-17 15:31:21 +0500

    log added

---
+++ fuel/app/logs/2013/05/17.php
@@ -0,0 +1,7 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2013-05-17 15:29:55 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-05-17 15:29:58 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-05-17 15:30:00 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-05-17 15:30:35 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-05-17 15:30:37 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
