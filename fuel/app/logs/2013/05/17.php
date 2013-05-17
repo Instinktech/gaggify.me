0b26228b6e01aed8ed816f78cebfbe0390a6dc09
ArkeologeN <hamzawaqas.programmer@gmail.com>
2013-05-17 21:22:00 +0500

    merge for pull

---
+++ fuel/app/logs/2013/05/17.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2013-05-17 21:21:13 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
